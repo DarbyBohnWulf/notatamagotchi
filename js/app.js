8a79ba2fe9d86deccdde1207f9d0cd394814e497
Mark Ritchie <mwritchie2@gmail.com>
2019-10-01 15:51:19 -0500

    created Tamagotchi class

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('Feed me, Seymour!');
+class Tamagotchi {
+  constructor() {
+    this.name = 'Tamago Jūnin';
+    this.hunger = 0;
+    this.boredom = 0;
+    this.sleepiness = 0;
+    this.age = 0;
+  }
+}
+
+const test = new Tamagotchi();
