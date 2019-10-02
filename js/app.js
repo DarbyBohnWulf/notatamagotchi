d4266f23ff474e1e04a0714aaaccc2d9f00190a3
Mark Ritchie <mwritchie2@gmail.com>
2019-10-02 15:09:31 -0500

    tama hatches now
    Added some comments on lines that looked arcane already, and refactored some
    things that were set hastily. Game is almost a game.

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ class Tamagotchi {
     this.boredom = 6;
     this.sleepiness = 6;
     this.age = 0;
+    this.face = game.faces[0];
   }
 }
 
@@ -12,52 +13,66 @@ const game = {
   playerName: '',
   playerBirthdate: '',
   _timer: null,
+  timeElapsed: 0,
   hunger: document.querySelector('#hunger'),
   sleep: document.querySelector('#sleepiness'),
   boredom: document.querySelector('#boredom'),
   tamaElem: document.querySelector('#display p'),
   _tamaSan: undefined,
+  faces: [
+    '👶','👧','🧒','👦','👩','🧑','👨','👵','🧓','👴'
+  ],
   // a setter to trigger stat display on creation of pet
   set tamaSan(tama) {
     this._tamaSan = tama;
     this.updateMeters();
   },
   // a setter to prevent unauthorized time changes
-  set time(newTime) {
+  set timer(newTime) {
+    // check to see if _timer already started i.e. game already in progress
     if (!this._timer) {
-      this._timer = 0
+      // starts a timer or gives an annoying alert
+      // this._timer = setInterval(game.tick, 1000);
     } else alert('Time set at beginning of universe.');
   },
   birthTamagotchi(name = '') {
     this.tamaSan = new Tamagotchi(name);
-    this.beginTimer();
+    setTimeout(this.beginTimer(), 200);
   },
-  beginTimer() {},
+  beginTimer() {
+    this._timer = setInterval(game.tick, 1000);
+  },
   startGame(name,bday) {
     this.playerName = name;
     this.playerBirthdate = bday ? bday : '1996-09-06';
     // this.updateDisplay();
-  },
-  initialize() {
-    const name = document.querySelector('#name');
-    const bday = document.querySelector('#birthdate');
     const form = document.querySelector('#game form');
+    const bdayField = document.querySelector('#birthdate');
     const gameArea = document.querySelector('#game');
-    game.startGame(name.value,bday.value);
-    form.removeChild(bday);
-    // removing anonymous listeners is ~impossible in vanill JS so..
+    form.removeChild(bdayField);
     const newForm = form.cloneNode(true);
     gameArea.removeChild(form);
     gameArea.prepend(newForm);
     this.addNameListener(newForm);
   },
+  // sets the scene to start the game
+  initialize() {
+    const name = document.querySelector('#name');
+    const bday = document.querySelector('#birthdate');
+    const form = document.querySelector('#game form');
+    game.startGame(name.value,bday.value);
+    // removing anonymous listeners is ~impossible in vanill JS so..
+    // to change the form from being for your name to being for the pet's name
+  },
+  // method to stop pet from reaching back and messing with things
   updateMeters() {
     this.hunger.textContent = this._tamaSan.hunger;
     this.sleep.textContent = this._tamaSan.sleepiness;
     this.boredom.textContent = this._tamaSan.boredom;
   },
   updateDisplay() {
-
+    game.tamaElem.textContent = game._tamaSan.face;
+    this.updateMeters();
   },
   addNameListener(elem) {
     elem.addEventListener('submit', e => {
@@ -65,6 +80,11 @@ const game = {
       game.birthTamagotchi(document.querySelector('#name').value);
       document.querySelector('#game').removeChild(e.currentTarget);
     });
+  },
+  tick() {
+    game.updateDisplay();
+    game.timeElapsed++;
+    // game._tamaSan.live();
   }
 }
 
